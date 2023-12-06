362a6b1817d31d8f0510670b0d25c71df40db4af
fortuinp <pinda.fortuin@gmail.com>
2023-12-06 17:16:28 +0200

    checkout

--- script/checkout.js
+++ script/checkout.js
@@ -3,8 +3,21 @@ let yr= new Date().getFullYear();
 
 document.getElementById('Curryearr').textContent= "Juicy Smoothies est."+yr
 
-let checkeditem=JSON.parse(localStorage.getItem("checkout"))
-console.log(checkeditem);
+function display() {
+    let checkeditem=JSON.parse(localStorage.getItem("checkout"))
+    console.log(checkeditem);
+    let checks= document.querySelector('[data-checkout]')
+    checks.innerHtml = "";
+    checkeditem.forEach((item) => {
+        console.log(item.flavour);
+        checks.innerHTML += 
+        
+        `
+            <h1>${item.price}</h1>
+        `
+})}
+
+display()
 
 // let checkitem =  JSON.parse(localStorage.getItem("checkout")) ?
 // JSON.parse(localStorage.getItem("checkout")) :
